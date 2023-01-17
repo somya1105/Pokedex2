c45715614f1aa44f1f800a3f707bc080146b6df7
Somya Srivastava <somyasrivastava1105s@gmail.com>
2023-01-18 02:18:52 +0530

    Final changes

--- server/index.js
+++ server/index.js
@@ -4,11 +4,32 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 const path = require('path');
+const axios = require('axios');
+const { response } = require("express");
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
+    next();
+})
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
 	res.json({ message: "Hello from server!" });
+});
+
+app.get("/getPokemon", (req, res) => {
+   
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+        .then(response => {
+            console.log(response.data);
+            res.json({ data: response.data });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
 });
 
 
@@ -16,6 +37,6 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+///app.get('*', (req, res) => {
+	///res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+//});
