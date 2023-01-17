6c9d67ffa961213ebdf9bfabb581702747fc4d3a
Somya Srivastava <somyasrivastava1105s@gmail.com>
2023-01-17 19:19:27 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
 	res.json({ message: "Hello from server!" });
@@ -13,3 +16,6 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
